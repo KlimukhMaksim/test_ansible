6cd950d3236ffc293d78b57fa24aa4fb3d9daad6
Maks Klimukh <klimukhmaksim@gmail.com>
2026-06-28 17:20:38 +0300

    update app

--- BirdWatchingApp/app.py
+++ BirdWatchingApp/app.py
@@ -3,13 +3,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-photo = os.getenv("BIRD_PHOTO")
-description = os.getenv("BIRD_NAME")
-location = os.getenv("LOCATION")
+photo = "photo1"
+description = "Ara"
+location = "Rio"
+
 
 @app.route('/')
 def index():
     return render_template('index.html', photo=photo, description=description, location=location)
+
 
 if __name__ == "__main__":
     app.run()
